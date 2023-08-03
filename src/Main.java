b92baedec1a7cef1340b03167f9f027b0f10b2a3
illyxep <alseregin2015@gmail.com>
2023-08-03 20:40:55 +0300

    Задачи 1 и 2

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+        byte a = 123;
+        short b = 12345;
+        int c = 1234567890;
+        long d = 123456L;
+        float e = 123.456F;
+        double f = 1234.056789;
+        System.out.println("Значение переменной a с типом " + " bite " + " равно " + a);
+        System.out.println("Значение переменной b с типом " + " short " + " равно " + b);
+        System.out.println("Значение переменной c с типом " + " int " + " равно " + c);
+        System.out.println("Значение переменной d с типом " + " long " + " равно " + d);
+        System.out.println("Значение переменной e с типом " + " float " + " равно " + e);
+        System.out.println("Значение переменной f с типом " + " double " + " равно " + f);
+         // Пишем код для задачи 1
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+        // Пишем код для задачи 2
+        float e = 27.12F;
+        long d = 987678965549L;
+        double f = 2.786;
+        short b = 569;
+        byte a = 67;
+        short g = -159;
+        int c = 27897;
+        System.out.println("Значение переменной e с типом " + " float " + " равно " + e);
+        System.out.println("Значение переменной d с типом " + " long " + " равно " + d);
+        System.out.println("Значение переменной f с типом " + " double " + " равно " + f);
+        System.out.println("Значение переменной b с типом " + " short " + " равно " + b);
+        System.out.println("Значение переменной g с типом " + " double " + " равно " + g);
+        System.out.println("Значение переменной c с типом " + " int " + " равно " + c);
+        System.out.println("Значение переменной a с типом " + " bite " + " равно " + a);
+
+    }
+}
